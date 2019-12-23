e1d74df140aa82165ab6656070bfa84639e9b17c
SawdaO <sawda.onanuga@gmail.com>
2019-12-23 00:21:35 +0000

    this is the app, without a few steps

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -22,17 +22,23 @@ const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
 // CODELAB: Add event listener for beforeinstallprompt event
-
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 /**
  * Event handler for beforeinstallprompt event.
  *   Saves the event & shows install button.
  *
+ * 
  * @param {Event} evt
  */
+
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
-
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.setAttribute('hidden', true);
 }
 
 
@@ -45,11 +51,19 @@ function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
 
   // CODELAB: Log user response to prompt.
-
+  deferredInstallPrompt.userChoice
+    .then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choice);
+      } else {
+        console.log('User dismissed the A2HS prompt', choice);
+      }
+      deferredInstallPrompt = null;
+    });
 }
 
 // CODELAB: Add event listener for appinstalled event
-
+window.addEventListener('appinstalled', logAppInstalled);
 /**
  * Event handler for appinstalled event.
  *   Log the installation to analytics or save the event somehow.
